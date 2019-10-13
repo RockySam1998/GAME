9ef54726dea9f92e17b385903693305cd4705293
SACHDEVHITESH <hiteshsachdev1998@gmail.com>
2019-10-13 12:51:39 +0530

    Main Activity

--- Game/app/src/main/java/com/example/game/MainActivity.java
+++ Game/app/src/main/java/com/example/game/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.game;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
     }
+
+    public void startGame(View view) {
+        Intent i1=new Intent(this,startGame.class);
+        startActivity(i1);
+    }
 }
+
