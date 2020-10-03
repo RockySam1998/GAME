4dedf76b9cc6a8d45daf37b7c8d65a21cc6892e9
Hitesh <hitesh.sachdev16@vit.edu>
2020-10-03 11:54:18 +0530

    Added few changes

--- Game/app/src/main/java/com/example/game/MainActivity.java
+++ Game/app/src/main/java/com/example/game/MainActivity.java
@@ -14,6 +14,10 @@ public class MainActivity extends Activity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
     }
+    public void startGame(View view) {
+        Intent i1=new Intent(this,startGame.class);
+        startActivity(i1);
+    }
 
 }
 
